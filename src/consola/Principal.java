499c766bfbb2029fee155def2ab1ec22a8b3a3f7
Miguel-Ike <Miguel_Angel-M@outlook.com>
2016-12-08 22:14:27 -0400

    Pruebas de los metodos de busqueda y ordenamiento

--- src/consola/Principal.java
+++ src/consola/Principal.java
@@ -118,6 +118,142 @@ public class Principal {
 		System.out.println("    --> " + a.toString());
 		System.out.println("\n");//salto de linea
 		
+		System.out.println("--> Creando una objeto b de tipo Vector con capacidad de 16 elemenots");
+		Vector b = new Vector(16);
+		System.out.println("    --> Dimencion: "+b.getDim());
+		System.out.println("    --> Maximo de elementos: "+b.getMax());
+		System.out.println("    --> " + b.toString());
+		System.out.println("\n");//salto de linea
+		
+		System.out.println("--> Agregando elementos en el vectro b en formato de string con separador espacio");
+		b.addElements("21 22 23 24 25 26 27 28");
+		System.out.println("    --> Dimencion: "+b.getDim());
+		System.out.println("    --> Maximo de elementos: "+b.getMax());
+		System.out.println("    --> " + b.toString());
+		System.out.println("\n");//salto de linea
+		
+		System.out.println("--> Invertir el vector b");
+		b.invert();
+		System.out.println("    --> Dimencion: "+b.getDim());
+		System.out.println("    --> Maximo de elementos: "+b.getMax());
+		System.out.println("    --> " + b.toString());
+		System.out.println("\n");//salto de linea
+		
+		System.out.println("--> Rotar el vector b 3 veces");
+		b.rotate(3);
+		System.out.println("    --> Dimencion: "+b.getDim());
+		System.out.println("    --> Maximo de elementos: "+b.getMax());
+		System.out.println("    --> " + b.toString());
+		System.out.println("\n");//salto de linea
+		
+		System.out.println("--> Busqueda secuencial en vector b e.g. 28");
+		System.out.println("    --> El elemento 28 se encuentra en: " + b.searchSeq(28));
+		System.out.println("    --> Dimencion: "+b.getDim());
+		System.out.println("    --> Maximo de elementos: "+b.getMax());
+		System.out.println("    --> " + b.toString());
+		System.out.println("\n");//salto de linea
+		
+		System.out.println("--> Rotar el vector b 5 veces e invertir");
+		b.rotate(5);
+		b.invert();
+		System.out.println("    --> Dimencion: "+b.getDim());
+		System.out.println("    --> Maximo de elementos: "+b.getMax());
+		System.out.println("    --> " + b.toString());
+		System.out.println("\n");//salto de linea
+		
+		System.out.println("--> Busqueda Binaria en vector b e.g. 23");
+		System.out.println("    --> El elemento 23 se encuentra en: " + b.searchSeq(23));
+		System.out.println("    --> Dimencion: "+b.getDim());
+		System.out.println("    --> Maximo de elementos: "+b.getMax());
+		System.out.println("    --> " + b.toString());
+		System.out.println("\n");//salto de linea
+		
+		
+		System.out.println("--> Creando una objeto c de tipo Vector con capacidad de elemenots por defecto (25)");
+		Vector c = new Vector();
+		System.out.println("    --> Dimencion: "+c.getDim());
+		System.out.println("    --> Maximo de elementos: "+c.getMax());
+		System.out.println("    --> " + c.toString());
+		System.out.println("\n");//salto de linea
+		
+		System.out.println("--> Agregando elementos en el vectro c en formato de string con separador espacio");
+		c.addElements("4 2 5 3 7 4 5 6 8 1 9 1");
+		System.out.println("    --> Dimencion: "+c.getDim());
+		System.out.println("    --> Maximo de elementos: "+c.getMax());
+		System.out.println("    --> " + c.toString());
+		System.out.println("\n");//salto de linea
+		
+		System.out.println("--> Ordenamiento por Burbuja");
+		c.bubbleSort();
+		System.out.println("    --> Dimencion: "+c.getDim());
+		System.out.println("    --> Maximo de elementos: "+c.getMax());
+		System.out.println("    --> " + c.toString());
+		System.out.println("\n");//salto de linea
+		
+		System.out.println("--> Creando una objeto d de tipo Vector con capacidad de elemenots por defecto (25)");
+		Vector d = new Vector();
+		System.out.println("    --> Dimencion: "+d.getDim());
+		System.out.println("    --> Maximo de elementos: "+d.getMax());
+		System.out.println("    --> " + d.toString());
+		System.out.println("\n");//salto de linea
+		
+		System.out.println("--> Agregando elementos en el vectro c en formato de string con separador espacio");
+		d.addElements("4 2 5 3 7 4 5 6 8 1 9 1");
+		System.out.println("    --> Dimencion: "+d.getDim());
+		System.out.println("    --> Maximo de elementos: "+d.getMax());
+		System.out.println("    --> " + d.toString());
+		System.out.println("\n");//salto de linea
+		
+		System.out.println("--> Ordenamiento por Insercion");
+		d.insertionSort();
+		System.out.println("    --> Dimencion: "+d.getDim());
+		System.out.println("    --> Maximo de elementos: "+d.getMax());
+		System.out.println("    --> " + d.toString());
+		System.out.println("\n");//salto de linea
+		
+		
+		System.out.println("--> Creando una objeto e de tipo Vector con capacidad de elemenots por defecto (25)");
+		Vector e = new Vector();
+		System.out.println("    --> Dimencion: "+e.getDim());
+		System.out.println("    --> Maximo de elementos: "+e.getMax());
+		System.out.println("    --> " + e.toString());
+		System.out.println("\n");//salto de linea
+		
+		System.out.println("--> Agregando elementos en el vectro c en formato de string con separador espacio");
+		e.addElements("4 2 5 3 7 4 5 6 8 1 9 1");
+		System.out.println("    --> Dimencion: "+e.getDim());
+		System.out.println("    --> Maximo de elementos: "+e.getMax());
+		System.out.println("    --> " + e.toString());
+		System.out.println("\n");//salto de linea
+		
+		System.out.println("--> Ordenamiento por seleccion");
+		e.selectionSort();
+		System.out.println("    --> Dimencion: "+e.getDim());
+		System.out.println("    --> Maximo de elementos: "+e.getMax());
+		System.out.println("    --> " + e.toString());
+		System.out.println("\n");//salto de linea
+		
+		System.out.println("--> Creando una objeto f de tipo Vector con capacidad de elemenots por defecto (25)");
+		Vector f = new Vector();
+		System.out.println("    --> Dimencion: "+f.getDim());
+		System.out.println("    --> Maximo de elementos: "+f.getMax());
+		System.out.println("    --> " + f.toString());
+		System.out.println("\n");//salto de linea
+		
+		System.out.println("--> Agregando elementos en el vectro f en formato de string con separador espacio");
+		f.addElements("4 2 5 3 7 4 5 6 8 1 9 1");
+		System.out.println("    --> Dimencion: "+f.getDim());
+		System.out.println("    --> Maximo de elementos: "+f.getMax());
+		System.out.println("    --> " + f.toString());
+		System.out.println("\n");//salto de linea
+		
+		System.out.println("--> Ordenamiento por shellSort");
+		f.shellSort();
+		System.out.println("    --> Dimencion: "+f.getDim());
+		System.out.println("    --> Maximo de eleentos: "+f.getMax());
+		System.out.println("    --> " + f.toString());
+		System.out.println("\n");//salto de linea
+		
 		
 		System.out.println("[=========================== Fin de la Class Vector ===========================]");
 		
