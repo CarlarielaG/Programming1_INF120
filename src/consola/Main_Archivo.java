6fc4efba585cd9b06655feafc583df0dca9f6c67
Miguel Angel M <Miguel_Angel-M@outlook.com>
2016-12-20 18:15:10 -0400

    Probando los nuevos metodos de inversion de archivos

--- src/consola/Main_Archivo.java
+++ src/consola/Main_Archivo.java
@@ -3,7 +3,9 @@ package consola;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+import negocio.Archivo;
 import negocio.Person;
 
 public class Main_Archivo {
@@ -15,22 +17,20 @@ public class Main_Archivo {
 
 	public static void main(String[] args) throws IOException {
 		// Notar que readLine() nos obliga a declarar IOException
-		// Reolector de datos de la consola
-		@SuppressWarnings("unused")
-		Main_Archivo archivo = new Main_Archivo();
+		Main_Archivo m = new Main_Archivo();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		Person person = new Person();
+		Archivo archivo = new Archivo();
 		while (running) {
-			System.out.println("---> Lista de opciones");
-			System.out.println("   ---> 1. Agregar una nueva persona");
-			System.out.println("   ---> 2. Listar personas");
-			System.out.println("   ---> 3. Cargar datos de Perona");
-			System.out.println("   ---> 4. Salir");
+			m.optionList();
+
 			System.out.print("---> Elija una opcion: ");
 			int key = Integer.parseInt(br.readLine());
+
 			switch (key) {
 			case 1:
 				System.out.println("\n---> Agragando datos de una nueva persona...");
+
 				System.out.print("  --> Nombre: ");
 				person.setName(br.readLine());
 
@@ -53,9 +53,25 @@ public class Main_Archivo {
 				person.setNationality(br.readLine());
 
 				person.saveDataPerson();
-
 				break;
 			case 2:
+
+				for (int i = 1; i <= 100; i++) {
+					String temp = archivo.readLine("ListPerson.txt", i);
+					StringTokenizer st = new StringTokenizer(temp, ":");
+
+					String linea = "";
+					int n = 1;
+					while (st.hasMoreTokens()) {
+						if (n == 7) {
+							linea += st.nextToken().trim();
+						} else {
+							linea += st.nextToken().trim() + ":";
+						}
+						n++;
+					}
+					archivo.write("ListPerson2.txt", linea);
+				}
 
 				break;
 			case 3:
@@ -65,6 +81,14 @@ public class Main_Archivo {
 				System.out.println("  --> Los datos son: " + person);
 				break;
 			case 4:
+				System.out.println("\n---> Invertir un archivo .txt");
+				System.out.print("  --> Nombre del archivo: ");
+				String nameFile = br.readLine();
+				archivo.invertFile(nameFile);
+				System.out.println("Se creo el archivo invertido");
+				break;
+
+			case 20:
 				running = false;
 				break;
 
@@ -75,4 +99,12 @@ public class Main_Archivo {
 		}
 	}
 
+	public void optionList() {
+		System.out.println("\n---> Lista de opciones");
+		System.out.println("   ---> 1. Agregar una nueva persona");
+		System.out.println("   ---> 2. Listar personas");
+		System.out.println("   ---> 3. Cargar datos de Perona");
+		System.out.println("   ---> 4. Invertir un Archivo");
+		System.out.println("   ---> 20. Salir");
+	}
 }
